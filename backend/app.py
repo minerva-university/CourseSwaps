4006492fe629eccfa5f0c325be85dc32a91ce8a0
lewiskyron <kyronnyoro@uni.minerva.edu>
2023-10-20 20:29:58 -0300

    created a routes.py for backend routing

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from routes import routes
 
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.register_blueprint(routes)
 
 
 db = SQLAlchemy(app)
---
+++ backend/routes.py
@@ -0,0 +1,4 @@
+from flask import Flask 
+from flask import blueprints
+
+routes = blueprints.Blueprint("routes", __name__)
