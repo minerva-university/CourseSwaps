f4e49881b9ec2f5d664e6094f0ba6c06e43a88b2
lewiskyron <kyronnyoro@uni.minerva.edu>
2023-11-03 22:33:27 -0300

    refactored the back-end and added testing framework to tes the back-end

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from routes import routes
-
-
-app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.register_blueprint(routes)
-
-
-db = SQLAlchemy(app)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+from API import create_app
 
 
 if __name__ == "__main__":
-    app.run(port=9000, debug=True)
+    app = create_app()
+    app.run(debug=True, port=7000)
