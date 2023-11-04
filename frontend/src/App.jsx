a876d93aed920c910b13ab13a7c4e735fff7ca82
Djordje Ristic <rist.djordje@gmail.com>
2023-11-03 23:08:52 -0300

    update app.js to show navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import AuthPage from "./views/AuthPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
-import UserformPage from "./views/userFormPage";
+import UserformPage from "./views/UserFormPage";
 import ExchangePage from "./views/ExchangeCoursesPage";
 import ApiProvider from "./contexts/ApiProvider";
 import Navbar from "./components/Navbar/Navbar";
@@ -12,12 +12,12 @@ function App() {
     <ApiProvider>
       <UserProvider>
         <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<ExchangePage />} />
-              <Route path="/auth" element={<AuthPage />} />
-              <Route path="/userform" element={<UserformPage />} />
-              <Route path="/navbar" element={<Navbar />} /> 
-            </Routes>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ExchangePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/userform" element={<UserformPage />} />
+          </Routes>
         </BrowserRouter>
       </UserProvider>
     </ApiProvider>
