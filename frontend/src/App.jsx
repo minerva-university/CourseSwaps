d10585e23f98a02d5e66dafbd54b25cbe8486a67
Djordje Ristic <rist.djordje@gmail.com>
2023-11-03 22:29:11 -0300

    added components for the main exchange courses page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import AuthPage from "./views/AuthPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserformPage from "./views/userFormPage";
 import { UserProvider } from "./contexts/UserContext";
+import UserformPage from "./views/userFormPage";
+import ExchangePage from "./views/ExchangeCoursesPage";
 import ApiProvider from "./contexts/ApiProvider";
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
     <ApiProvider>
       <UserProvider>
         <BrowserRouter>
-            {/* <h1>Course Swap</h1> */}
             <Routes>
-              <Route path="/" element={<AuthPage />} />
+              <Route path="/" element={<ExchangePage />} />
+              <Route path="/auth" element={<AuthPage />} />
               <Route path="/userform" element={<UserformPage />} />
             </Routes>
         </BrowserRouter>
