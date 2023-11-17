f6133bc71774897e413ba5df0185bd6bf167cb78
Khaled Ahmed <kmhsn217@gmail.com>
2023-11-17 16:49:58 -0300

    created endpoint for courses availble for pickup

--- backend/API/blueprints/mycourses.py
+++ backend/API/blueprints/mycourses.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 
-from ..models import Users, db, Courses, UserCurrentCourses, UserCompletedCourses
+from ..models import Users, db, Courses, UserCurrentCourses, UserCompletedCourses, CoursesAvailableForPickup
 
 mycourses_bp = Blueprint("mycourses_bp", __name__)
 
@@ -64,4 +64,40 @@ def add_current_courses():
         # other things have to be figured out before this
     except Exception as e:
         print(e)
+        return jsonify({"error": "Something went wrong"}), 500
+
+
+@mycourses_bp.route("/for_pickup", methods=["GET"])
+def available_for_pickup():
+    """
+    Get all courses available for pickup for the current user
+    """
+    try:
+        # Fetching courses that are available for pickup and not taken or completed by the user
+        available_courses = (
+            db.session.query(Courses)
+            .join(CoursesAvailableForPickup, Courses.id == CoursesAvailableForPickup.course_id)
+            .outerjoin(UserCurrentCourses, (UserCurrentCourses.course_id == Courses.id) & (UserCurrentCourses.user_id == current_user.id))
+            .outerjoin(UserCompletedCourses, (UserCompletedCourses.course_id == Courses.id) & (UserCompletedCourses.user_id == current_user.id))
+            .filter(UserCurrentCourses.id.is_(None))
+            .filter(UserCompletedCourses.id.is_(None))
+            .all()
+        )
+
+        course_data = [
+            {
+                "id": course.id,
+                "name": course.name,
+                "code": course.code,
+                "time": course.time,
+                "prerequisites": course.prerequisites
+            }
+            for course in available_courses
+        ]
+
+        return jsonify({"available_courses": course_data}), 200
+
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"Error: {e}")
         return jsonify({"error": "Something went wrong"}), 500
