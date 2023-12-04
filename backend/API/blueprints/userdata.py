cf9f85ab44facc07fd533480cf05870e797000e9
Nathe Seas <natheseasn@Nathes-MacBook-Air.local>
2023-12-03 23:00:38 -0300

    fixed the form issue/update endpoint

--- backend/API/blueprints/userdata.py
+++ backend/API/blueprints/userdata.py
@@ -115,7 +115,7 @@ def get_user_data():
         return jsonify({"error": str(e)}), 500
 
 
-# endpoint to update user data
+# endpoint to update user data for the update profile page
 @userdata_bp.route("/update-user", methods=["PUT"])
 @login_required
 def update_user():
@@ -129,48 +129,43 @@ def update_user():
         user.class_year = data.get("class", user.class_year)
         user.major = data.get("major", user.major)
         user.minerva_id = data.get("minervaID", user.minerva_id)
-        # user.concentration = data.get("concentration", user.concentration)
+        user.concentration = data.get("concentration", user.concentration)
         user.minor = data.get("minor", user.minor)
-        # Extract concentration value
-        concentration_value = data.get("concentration")
-        if isinstance(concentration_value, list):
-            concentration_value = concentration_value[0] if concentration_value else None
-        user.concentration = concentration_value or user.concentration
 
         # Update current courses
         new_current_courses = set(data.get("currentClasses", []))
-        existing_current_courses = set(course.course.code for course in user.current_courses)
-
-        # Add new current courses
-        for course_code in new_current_courses - existing_current_courses:
-            course = Courses.query.filter_by(code=course_code).first()
-            if course:
-                user.current_courses.append(UserCurrentCourses(user_id=user.id, course_id=course.id))
 
         # Remove old current courses
         for user_course in user.current_courses[:]:
-            if user_course.course.code not in new_current_courses:
+            if user_course.course is None or user_course.course.code not in new_current_courses:
                 db.session.delete(user_course)
+
+        # Add new current courses
+        for course_code in new_current_courses:
+            if all(course_code != user_course.course.code for user_course in user.current_courses if user_course.course is not None):
+                course = Courses.query.filter_by(code=course_code).first()
+                if course:
+                    user.current_courses.append(UserCurrentCourses(user_id=user.id, course_id=course.id))
 
         # Update previous courses
         new_previous_courses = set(data.get("previousCourses", []))
-        existing_previous_courses = set(course.course.code for course in user.completed_courses)
-
-        # Add new previous courses
-        for course_code in new_previous_courses - existing_previous_courses:
-            course = Courses.query.filter_by(code=course_code).first()
-            if course:
-                user.completed_courses.append(UserCompletedCourses(user_id=user.id, course_id=course.id))
 
         # Remove old previous courses
         for user_completed_course in user.completed_courses[:]:
-            if user_completed_course.course.code not in new_previous_courses:
+            if user_completed_course.course is None or user_completed_course.course.code not in new_previous_courses:
                 db.session.delete(user_completed_course)
+
+        # Add new previous courses
+        for course_code in new_previous_courses:
+            if all(course_code != user_completed_course.course.code for user_completed_course in user.completed_courses if user_completed_course.course is not None):
+                course = Courses.query.filter_by(code=course_code).first()
+                if course:
+                    user.completed_courses.append(UserCompletedCourses(user_id=user.id, course_id=course.id))
 
         db.session.commit()
         return jsonify({"success": "Profile updated successfully"}), 200
 
     except Exception as e:
         db.session.rollback()
-        # current_app.logger.error(f"Error in update_user: {e}")
+        current_app.logger.error(f"Error in update_user: {e}")
         return jsonify({"error": str(e)}), 500
