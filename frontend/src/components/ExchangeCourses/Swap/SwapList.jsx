6f25dc46cc3ed654019fd73ea0adaa467a933da7
fadyFH194 <131557935+fadyFH194@users.noreply.github.com>
2023-12-10 20:10:45 -0300

    "confirm swap" button and showed the swap data

--- frontend/src/components/ExchangeCourses/Swap/SwapList.jsx
+++ frontend/src/components/ExchangeCourses/Swap/SwapList.jsx
@@ -1,28 +1,40 @@
-import React, { useState, useEffect, useCallback } from "react";
-import Box from "@mui/material/Box";
-import CourseToSwapItem from "./CourseToSwapItem";
-import Typography from "@mui/material/Typography";
-import { useApi } from "../../../contexts/ApiProvider";
-import { useRefresh } from "../../../contexts/useRefresh";
-import { usePeriodicRefresh } from "../../../contexts/usePeriodicRefresh";
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Box, Typography, CircularProgress, Alert, Divider, Button, Dialog,
+  DialogActions, DialogContent, DialogTitle
+} from '@mui/material';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import { useApi } from '../../../contexts/ApiProvider';
+import { useRefresh } from '../../../contexts/useRefresh';
+import { usePeriodicRefresh } from '../../../contexts/usePeriodicRefresh';
 
 const SwapList = () => {
-  const [swaps, setSwaps] = useState([]); // Initialize as an empty array
+  const [swaps, setSwaps] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [selectedSwap, setSelectedSwap] = useState(null);
+
   const api = useApi();
   const { refreshKey } = useRefresh();
   const { subscribe, unsubscribe } = usePeriodicRefresh();
 
-  // useCallback to memoize the fetchSwaps function
   const fetchSwaps = useCallback(async () => {
+    setLoading(true);
+    setError(null);
     try {
       const response = await api.get('/availableswaps');
       if (response.ok && response.status === 200) {
-        setSwaps(response.body.swaps || []); // Fallback to empty array if undefined
+        setSwaps(response.body.available_swaps || []);
       } else {
         console.error('Failed to fetch swaps', response);
+        setError('Failed to fetch swap data.');
       }
     } catch (error) {
       console.error('Error fetching swaps:', error);
+      setError('An error occurred while fetching swaps.');
+    } finally {
+      setLoading(false);
     }
   }, [api]);
 
@@ -35,11 +47,40 @@ const SwapList = () => {
     return () => unsubscribe(fetchSwaps);
   }, [subscribe, unsubscribe, fetchSwaps]);
 
+  const handleSwap = (swap) => {
+    setSelectedSwap(swap);
+    setConfirmDialogOpen(true);
+  };
+
+  const handleConfirmSwap = async () => {
+    console.log(`Confirmed swap for ID: ${selectedSwap}`);
+    setConfirmDialogOpen(false);
+    // Implement additional logic for swap confirmation here
+  };
+
+  const ConfirmationDialog = () => (
+    <Dialog
+      open={confirmDialogOpen}
+      onClose={() => setConfirmDialogOpen(false)}
+    >
+      <DialogTitle>Confirm Swap</DialogTitle>
+      <DialogContent>
+        <Typography>
+          Are you sure you want to make this swap?
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setConfirmDialogOpen(false)}>Cancel</Button>
+        <Button onClick={handleConfirmSwap}>Confirm</Button>
+      </DialogActions>
+    </Dialog>
+  );
+
   return (
     <Box
       sx={{
         maxWidth: 360,
-        bgcolor: "#f0f0f0",
+        bgcolor: '#f0f0f0',
         padding: 2,
         borderRadius: 2,
         boxShadow: 3,
@@ -49,17 +90,64 @@ const SwapList = () => {
         variant="h5"
         component="h2"
         sx={{
-          fontWeight: "bold",
+          fontWeight: 'bold',
           marginBottom: 2,
-          textAlign: "center",
-          color: "#333",
+          textAlign: 'center',
+          color: '#333',
         }}
       >
         Swap Courses
       </Typography>
-      {swaps && swaps.map(swap => (
-        <CourseToSwapItem key={swap.id} username={swap.username} givesCourse={swap.courseGiven} wantsCourse={swap.courseWanted} />
-      ))}
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert severity="error">{error}</Alert>
+      ) : swaps.length === 0 ? (
+        <Typography
+          sx={{
+            textAlign: 'center',
+            color: '#666',
+          }}
+        >
+          No available swaps
+        </Typography>
+      ) : (
+        swaps.map((swap, index) => (
+          <Box
+            key={swap.swap_id}
+            sx={{
+              backgroundColor: "white",
+              marginBottom: 2,
+              borderRadius: 2,
+              padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              gap: 1,
+            }}
+          >
+            <Typography variant="body1">
+              <span style={{ fontWeight: 900 }}>Swap ID:</span> {swap.swap_id}
+            </Typography>
+            <Typography variant="body1">
+              <span style={{ fontWeight: 900 }}>Gives:</span> {swap.giving_course_code}: {swap.giving_course_name}
+            </Typography>
+            <Typography variant="body1">
+              <span style={{ fontWeight: 900 }}>Wants:</span> {swap.wanted_course_code}: {swap.wanted_course_name}
+            </Typography>
+            <Button
+              variant="contained"
+              startIcon={<SwapHorizIcon />}
+              sx={{ mt: 1, alignSelf: 'center' }}
+              onClick={() => handleSwap(swap.swap_id)}
+            >
+              Make Swap
+            </Button>
+            {index !== swaps.length - 1 && <Divider />}
+          </Box>
+        ))
+      )}
+      <ConfirmationDialog />
     </Box>
   );
 };
---
+++ frontend/tests/components/SwapList.test.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import SwapList from "../../src/components/ExchangeCourses/Swap/SwapList.jsx"; // Adjust the import path as necessary
+import { useApi } from "../../src/contexts/ApiProvider";
+
+// Mock the useApi hook
+jest.mock("../../src/contexts/ApiProvider", () => ({
+  useApi: jest.fn(),
+}));
+
+const mockSwaps = [
+  { swap_id: 1, user_id: '123', giving_course_code: 'MATH101', giving_course_name: 'Mathematics', wanted_course_code: 'PHY102', wanted_course_name: 'Physics' },
+  // Add more mock swap data as needed
+];
+
+describe("SwapList", () => {
+  beforeEach(() => {
+    useApi.mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        ok: true,
+        status: 200,
+        body: { available_swaps: mockSwaps },
+      }),
+    });
+  });
+
+  it("renders the component", async () => {
+    render(<SwapList />);
+    await waitFor(() =>
+      expect(screen.getByText("Swap Courses")).toBeInTheDocument()
+    );
+  });
+
+  it("displays loading indicator while fetching swaps", () => {
+    useApi.mockReturnValue({
+      get: jest.fn(() => new Promise(() => {})), // Never resolving promise to simulate loading
+    });
+    render(<SwapList />);
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  it("opens confirmation dialog on swap button click", async () => {
+    render(<SwapList />);
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByText('Make Swap')[0]);
+    });
+    expect(screen.getByText('Confirm Swap')).toBeInTheDocument();
+  });
+
+  it("handles swap action when confirm button is clicked", async () => {
+    render(<SwapList />);
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByText('Make Swap')[0]);
+    });
+    fireEvent.click(screen.getByText('Confirm'));
+    // Add assertions to verify the expected behavior after swap confirmation
+    // For example, you might check for a success message or that the dialog has closed
+  });
+
+  // Add more tests as needed
+});
