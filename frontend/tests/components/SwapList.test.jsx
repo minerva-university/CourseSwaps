8954cf6ed7a2d160fdbaa69284ed8df2f8550ec4
Emeruem Kennedy <69385717+Emeruem-Kennedy1@users.noreply.github.com>
2023-12-10 22:11:53 -0300

    Added a todo

--- frontend/tests/components/SwapList.test.jsx
+++ frontend/tests/components/SwapList.test.jsx
@@ -9,7 +9,14 @@ jest.mock("../../src/contexts/ApiProvider", () => ({
 }));
 
 const mockSwaps = [
-  { swap_id: 1, user_id: '123', giving_course_code: 'MATH101', giving_course_name: 'Mathematics', wanted_course_code: 'PHY102', wanted_course_name: 'Physics' },
+  {
+    swap_id: 1,
+    user_id: "123",
+    giving_course_code: "MATH101",
+    giving_course_name: "Mathematics",
+    wanted_course_code: "PHY102",
+    wanted_course_name: "Physics",
+  },
   // Add more mock swap data as needed
 ];
 
@@ -36,25 +43,24 @@ describe("SwapList", () => {
       get: jest.fn(() => new Promise(() => {})), // Never resolving promise to simulate loading
     });
     render(<SwapList />);
-    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+    expect(screen.getByRole("progressbar")).toBeInTheDocument();
   });
 
   it("opens confirmation dialog on swap button click", async () => {
     render(<SwapList />);
     await waitFor(() => {
-      fireEvent.click(screen.getAllByText('Make Swap')[0]);
+      fireEvent.click(screen.getAllByText("Make Swap")[0]);
     });
-    expect(screen.getByText('Confirm Swap')).toBeInTheDocument();
+    expect(screen.getByText("Confirm Swap")).toBeInTheDocument();
   });
 
   it("handles swap action when confirm button is clicked", async () => {
     render(<SwapList />);
     await waitFor(() => {
-      fireEvent.click(screen.getAllByText('Make Swap')[0]);
+      fireEvent.click(screen.getAllByText("Make Swap")[0]);
     });
-    fireEvent.click(screen.getByText('Confirm'));
-    // Add assertions to verify the expected behavior after swap confirmation
-    // For example, you might check for a success message or that the dialog has closed
+    fireEvent.click(screen.getByText("Confirm"));
+    // TODO: Add assertions to check if we get a successful response from the API
   });
 
   // Add more tests as needed
