c2ed63ceddc429a25af1b724e6f0c4ce56d1e3c1
fadyFH194 <131557935+fadyFH194@users.noreply.github.com>
2023-12-14 14:05:23 -0300

    added testing for MySwaps

---
+++ frontend/tests/components/MySwapList.test.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import MySwaps from "../../src/components/ExchangeCourses/MySwaps/MySwapsList.jsx";
+import { useApi } from "../../src/contexts/ApiProvider";
+
+jest.mock("../../src/contexts/ApiProvider", () => ({
+  __esModule: true,
+  default: ({ children }) => <div>{children}</div>,
+  useApi: jest.fn().mockReturnValue({
+    get: jest.fn(() => Promise.resolve({
+      ok: true,
+      status: 200,
+      body: { my_swaps: [{ swap_id: '1', giving_course: 'Course A', wanted_course: 'Course B' }] }
+    }))
+  }),
+}));
+
+jest.mock("../../src/contexts/useRefresh", () => ({
+  useRefresh: jest.fn().mockReturnValue({ refreshKey: 0 }),
+}));
+
+jest.mock("../../src/contexts/usePeriodicRefresh", () => ({
+  usePeriodicRefresh: jest.fn().mockReturnValue({
+    subscribe: jest.fn(),
+    unsubscribe: jest.fn(),
+  }),
+}));
+
+describe("MySwaps Component", () => {
+  it("renders correctly and fetches swaps", async () => {
+    render(<MySwaps />);
+
+    await waitFor(() => {
+      expect(screen.getByText("My preferred swaps")).toBeInTheDocument();
+      expect(screen.getByText("Course A")).toBeInTheDocument();
+    });
+  });
+
+  it("handles API errors gracefully", async () => {
+    useApi.mockReturnValue({
+      get: jest.fn(() => Promise.resolve({
+        ok: false,
+        status: 400,
+        body: {}
+      }))
+    });
+
+    render(<MySwaps />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Failed to fetch swaps")).toBeInTheDocument();
+    });
+  });
+});
---
+++ frontend/tests/components/SwapItem.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import SwapItem from '../../src/components/ExchangeCourses/MySwaps/SwapItem';
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import ApiProvider from "../../src/contexts/ApiProvider"; 
+
+
+jest.mock("../../src/contexts/ApiProvider", () => ({
+  __esModule: true,
+  default: ({ children }) => <div>{children}</div>,
+  useApi: jest.fn().mockReturnValue({
+    post: jest.fn((url) => {
+      if (url === "/cancel_swap/1") {
+        // Mock response for cancel_swap endpoint
+        return Promise.resolve({ ok: true });
+      }
+      return Promise.resolve({ ok: false });
+    }),
+  }),
+}));
+
+describe("SwapItem Component", () => {
+  it("should handle swap deletion", async () => {
+    const mockOnSwapDeleted = jest.fn();
+    const { getByText } = render(
+      <ApiProvider>
+        <SwapItem
+          swapId="1"
+          givingCourse="Course A"
+          wantedCourse="Course B"
+          setOpenSnackbar={() => {}}
+          setSnackbarMessage={() => {}}
+          setSnackbarSeverity={() => {}}
+          onSwapDeleted={mockOnSwapDeleted}
+        />
+      </ApiProvider>
+    );
+
+    fireEvent.click(getByText("Drop"));
+
+    await waitFor(() => getByText("Confirm Deletion"));
+
+    fireEvent.click(getByText("Confirm"));
+
+    await waitFor(() => {
+      expect(mockOnSwapDeleted).toHaveBeenCalledWith("1");
+    });
+  });
+
+});
