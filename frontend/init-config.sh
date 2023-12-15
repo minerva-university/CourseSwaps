978c35ce42379c4cbca4801ab1ea71cc1c9c14d9
Emeruem Kennedy <69385717+Emeruem-Kennedy1@users.noreply.github.com>
2023-12-15 06:59:49 -0300

    debugging

--- frontend/init-config.sh
+++ frontend/init-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+echo "GOOGLE_CLIENT_ID: ${GOOGLE_CLIENT_ID}"
 # Replace the placeholder in the built files
 sed -i "s|default_client_id|${GOOGLE_CLIENT_ID}|g" /usr/share/nginx/html/js/*.js
 
